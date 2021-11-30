5cce4eeb8f7f996d2aa24d8699deffb36478ade9
Yuri Andriaccio <yurand2000@gmail.com>
2021-11-30 11:36:53 +0100

    MakePGreatAgain Version 1.0

---
+++ Data/Scripts/DisableDefaultParking/Config.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Sandbox.ModAPI;
+using VRage.Utils;
+
+namespace Yurand.MakePGreatAgain
+{
+    public class Config
+    {
+        public bool enable_wheel_parking;
+        public bool enable_connector_parking;
+        public bool enable_landing_gear_parking;
+        public Config()
+        {
+            enable_wheel_parking = false;
+            enable_connector_parking = false;
+            enable_landing_gear_parking = false;
+        }
+        public static Config deserialize(string serialized_file)
+        {
+            Config read_config = null;
+            try
+            {
+                var reader = MyAPIGateway.Utilities.ReadFileInWorldStorage(serialized_file, typeof(Config));
+                string xml = reader.ReadToEnd();
+                reader.Close();
+                read_config = MyAPIGateway.Utilities.SerializeFromXML<Config>(xml);
+                MyLog.Default.WriteLine("[MakePGreatAgain] Successfully deserialized the file: " + serialized_file);
+            }
+            catch(Exception e)
+            {
+                read_config = new Config();
+                MyLog.Default.WriteLine("[MakePGreatAgain] Couldn't deserialize the file: " + serialized_file + " Exception: " + e.Message);
+            }
+            return read_config;
+        }
+
+        public static void serialize(Config config, string serialized_file)
+        {
+            try
+            {
+                string xml = MyAPIGateway.Utilities.SerializeToXML(config);
+                var writer = MyAPIGateway.Utilities.WriteFileInWorldStorage(serialized_file, typeof(Config));
+                writer.Write(xml);
+                writer.Close();
+                MyLog.Default.WriteLine("[MakePGreatAgain] Successfully serialized the file: " + serialized_file);
+            }
+            catch (Exception e)
+            {
+                MyLog.Default.WriteLine("[MakePGreatAgain] Couldn't serialize the file: " + serialized_file + " Exception: " + e.Message);
+            }
+        }
+    }
+}
---
+++ Data/Scripts/DisableDefaultParking/DisableDefaultParkingComponent.cs
@@ -0,0 +1,92 @@
+﻿using Sandbox.ModAPI;
+using SpaceEngineers.Game.ModAPI;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using Yurand.MakePGreatAgain;
+
+namespace Yurand.MakePGreatAgain
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class DisableDefaultParkingComponent : MySessionComponentBase
+    {
+        private static string default_configuration_file = "disable_default_parking_config.xml";
+        private Config configuration;
+        public override void LoadData()
+        {
+            if(MyAPIGateway.Multiplayer.IsServer)
+            {
+                MyAPIGateway.Entities.OnEntityAdd += EntityAdded;
+            }
+
+            configuration = Config.deserialize(default_configuration_file);
+            Config.serialize(configuration, default_configuration_file);
+        }
+
+        protected override void UnloadData()
+        {
+            if (MyAPIGateway.Multiplayer.IsServer)
+            {
+                MyAPIGateway.Entities.OnEntityAdd -= EntityAdded;
+            }
+        }
+
+        private void EntityAdded(IMyEntity ent)
+        {
+            var grid = ent as IMyCubeGrid;
+
+            if (grid != null)
+            {
+                grid.OnBlockAdded += OnBlockAdded;
+                grid.OnMarkForClose += OnMarkForClose;
+            }
+        }
+
+        private void OnMarkForClose(IMyEntity obj)
+        {
+            var grid = obj as IMyCubeGrid;
+            
+            if(grid != null)
+            {
+                grid.OnBlockAdded -= OnBlockAdded;
+                grid.OnMarkForClose -= OnMarkForClose;
+            }
+        }
+
+        private void OnBlockAdded(IMySlimBlock obj)
+        {
+            IMyCubeBlock block = obj.FatBlock;
+            if (block != null)
+            {
+                if (block is IMyMotorSuspension)
+                {
+                    disableWheelParking(block as IMyMotorSuspension);
+                }
+                else if(block is IMyShipConnector)
+                {
+                    disableConnectorParking(block as IMyShipConnector);
+                }
+                else if (block is IMyLandingGear)
+                {
+                    disableLandingGearParking(block as IMyLandingGear);
+                }
+            }
+        }
+
+        private void disableWheelParking(IMyMotorSuspension block)
+        {
+            block.IsParkingEnabled = configuration.enable_wheel_parking;
+        }
+        private void disableConnectorParking(IMyShipConnector block)
+        {
+            if(block.OtherConnector == null)
+            {
+                block.IsParkingEnabled = configuration.enable_connector_parking;
+            }
+        }
+        private void disableLandingGearParking(IMyLandingGear block)
+        {
+            block.IsParkingEnabled = configuration.enable_landing_gear_parking;
+        }
+    }
+}
